575f7a0a0c8cff457d32231b02f32779d402ada8
w-i-l <masterpaper01@gmail.com>
2024-07-28 20:40:42 +0300

    fixed minor header bug

--- src/headers/coder_multiprocess.h
+++ src/headers/coder_multiprocess.h
@@ -3,6 +3,6 @@
 
 #include "segmentation.h"
 
-int encode_multiprocess(const char* input_file, const char* permutations_filename, const char* encoded_filename, process_segment_info* segments, int no_of_processes) {
+int encode_multiprocess(const char* input_file, const char* permutations_filename, const char* encoded_filename, process_segment_info* segments, int no_of_processes);
 size_t round_to_page_size(size_t size);
 #endif
